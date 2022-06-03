acda809bbe070806bb600af8d4b0399bd0b1512e
Juliya Trifonova <yula_sakura@list.ru>
2022-06-03 12:08:16 +0300

    Commit

--- src/js/__tests__/app_test.js
+++ src/js/__tests__/app_test.js
@@ -1,8 +1,13 @@
-// import '../app';
 import indicator, { heroWizard, heroMaster, heroWitch } from '../app';
 
 test('Определение состояния здоровья героя', () => {
   expect(indicator(heroWizard)).toBe('Маг healthy');
+});
+
+test('Определение состояния здоровья героя', () => {
   expect(indicator(heroMaster)).toBe('Мастер wounded');
+});
+
+test('Определение состояния здоровья героя', () => {
   expect(indicator(heroWitch)).toBe('Колдунья critical');
 });
